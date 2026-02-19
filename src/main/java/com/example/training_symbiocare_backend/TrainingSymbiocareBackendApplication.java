f814dfbb2c5caa449308c3d671a408f6deb14dea
Giuseppe Sindoni <giuseppe.sindoni@emeal.nttdata.com>
2026-02-19 10:29:56 +0100

    first commit

---
+++ src/main/java/com/example/training_symbiocare_backend/TrainingSymbiocareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.training_symbiocare_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSymbiocareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingSymbiocareBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/training_symbiocare_backend/TrainingSymbiocareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.training_symbiocare_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingSymbiocareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
